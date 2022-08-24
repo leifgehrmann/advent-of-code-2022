e76fe5b966952ac0cb5f80ef9932ef7f39495e42
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-08-24 23:26:10 +0100

    Implemented day selector

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
+mod day_01_sonar_sweep;
+mod day_02_dive;
+
 fn main() {
-    let day: String = std::env::args().nth(1).expect("no day given");
+    let day: String = std::env::args().nth(1).expect(
+        "No day given. Possible options are: 01-25."
+    );
+    let day_slice: &str = day.as_str();
 
-    let mut message = "Hello, ".to_owned();
-    message = format!("{message}{day}");
-
-    println!("{}", message);
+    match day_slice {
+        "01" => day_01_sonar_sweep::run(),
+        "02" => day_02_dive::run(),
+        _ => println!("No valid day given. Possible options are: 01-25."),
+    };
 }
