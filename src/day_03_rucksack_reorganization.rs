a952f6e6aefe1b80c7a867b5b3bea103958c0bd9
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-12-03 08:32:59 +0000

    Day 03 - Part 2 solved

--- src/day_03_rucksack_reorganization.rs
+++ src/day_03_rucksack_reorganization.rs
@@ -26,10 +26,29 @@ fn part1(rucksacks: &Vec<&str>) {
     println!("Part 1: {}", priority_sum);
 }
 
+fn part2(rucksacks: &Vec<&str>) {
+    let mut priority_sum = 0;
+    
+    for group_index in (0..rucksacks.len()).step_by(3) {
+        let rucksack_1 = rucksacks[group_index];
+        let rucksack_2 = rucksacks[group_index + 1];
+        let rucksack_3 = rucksacks[group_index + 2];
+        for c in rucksack_1.chars() {
+            if rucksack_2.contains(c) && rucksack_3.contains(c) {
+                priority_sum += char_to_priority(c);
+                break
+            }
+        }
+    }
+
+    println!("Part 2: {}", priority_sum);
+}
+
 pub fn run() {
     let input = input_reader::read_file_in_cwd("src/day_03_input.txt");
 
     let rucksacks: Vec<&str> = input.split('\n').collect();
 
-    part1(&rucksacks)
+    part1(&rucksacks);
+    part2(&rucksacks);
 }
