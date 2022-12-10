45635f7f3a70d3b73456d121ca3d78837ba379bd
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-12-10 23:14:22 +0000

    Part 2 Solved

--- src/day_10.rs
+++ src/day_10.rs
@@ -1,28 +1,46 @@
 use crate::input_reader;
 
-fn part1(instructions: Vec<&str>) {
+fn part1(instructions: Vec<&str>) -> Vec<i32> {
     let mut x = 1;
-    let mut x_state: Vec<i32> = vec![];
+    let mut x_states: Vec<i32> = vec![];
 
-    x_state.push(x);
+    x_states.push(x);
 
     for instruction in instructions {
         let split: Vec<&str> = instruction.split(" ").collect();
         let op = split[0];
         if op == "noop" {
-            x_state.push(x);
+            x_states.push(x);
         } else if op == "addx" {
-            x_state.push(x);
-            x_state.push(x);
+            x_states.push(x);
+            x_states.push(x);
             x += split[1].parse::<i32>().unwrap();
         }
     }
 
     let mut interesting_signal_strength = 0;
     for c in (20..240).step_by(40) {
-        interesting_signal_strength += c as i32 * x_state[c];
+        interesting_signal_strength += c as i32 * x_states[c];
     }
     println!("Part 1: {}", interesting_signal_strength);
+
+    return x_states
+}
+
+fn part2(states: Vec<i32>) {
+    println!("Part 2:");
+    for c in 1..states.len() {
+        let x_pos = ((c - 1) % 40) as i32;
+        if x_pos >= states[c] - 1 && x_pos <= states[c] + 1 {
+            print!("#")
+        } else {
+            print!(".")
+        }
+
+        if (c - 1) % 40 == 39 {
+            print!("\n")
+        }
+    }
 }
 
 pub fn run() {
@@ -30,5 +48,6 @@ pub fn run() {
 
     let lines: Vec<&str> = input.split("\n").collect();
 
-    part1(lines)
+    let states = part1(lines);
+    part2(states);
 }
