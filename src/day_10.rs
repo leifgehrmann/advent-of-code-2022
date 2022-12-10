66f2d2b55ab371b792fb56924d264fcb7de3492a
Leif Gehrmann <leif.gehrmann@gmail.com>
2022-12-10 22:50:45 +0000

    Part 1 Solved

--- src/day_10.rs
+++ src/day_10.rs
@@ -1,7 +1,34 @@
 use crate::input_reader;
 
+fn part1(instructions: Vec<&str>) {
+    let mut x = 1;
+    let mut x_state: Vec<i32> = vec![];
+
+    x_state.push(x);
+
+    for instruction in instructions {
+        let split: Vec<&str> = instruction.split(" ").collect();
+        let op = split[0];
+        if op == "noop" {
+            x_state.push(x);
+        } else if op == "addx" {
+            x_state.push(x);
+            x_state.push(x);
+            x += split[1].parse::<i32>().unwrap();
+        }
+    }
+
+    let mut interesting_signal_strength = 0;
+    for c in (20..240).step_by(40) {
+        interesting_signal_strength += c as i32 * x_state[c];
+    }
+    println!("Part 1: {}", interesting_signal_strength);
+}
+
 pub fn run() {
     let input = input_reader::read_file_in_cwd("src/day_10.data");
 
     let lines: Vec<&str> = input.split("\n").collect();
+
+    part1(lines)
 }
